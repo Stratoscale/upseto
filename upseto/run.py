eadf20706d37e06be1695460ea09b7bdc6413d30
Shlomi Matichin <shlomi@stratoscale.com>
2015-01-19 15:19:45 +0200

    Bugfix: subprocess.CalledProcessError replaced with local error class

--- upseto/run.py
+++ upseto/run.py
@@ -1,11 +1,20 @@
 import subprocess
 
 
+class RunError(Exception):
+    def __init__(self, calledProcessError, cwd):
+        self._calledProcessError = calledProcessError
+        self._cwd = cwd
+
+    def __str__(self):
+        return str(self._calledProcessError) + " in '%s'. Output was:\n%s" % (
+            self._cwd, self._calledProcessError.output)
+
+
 def run(command, cwd=None):
     try:
         return subprocess.check_output(
             command, cwd=cwd, stderr=subprocess.STDOUT,
             stdin=open("/dev/null"), close_fds=True)
     except subprocess.CalledProcessError as e:
-        raise subprocess.CalledProcessError("Failed command '%s': %s in '%s' output:\n%s" % (
-            command, e, cwd, e.output))
+        raise RunError(e, cwd)
