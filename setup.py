ef108e05e3e23f9641ebb190f04c3ab32c0c8bc5
Yogev Cohen <yogev@stratoscale.com>
2016-06-20 06:47:36 +0000

    Update the setup.py file to use pbr. Add requirements.txt file. add setup.cfg file also.

--- setup.py
+++ setup.py
@@ -1,40 +1,10 @@
-import os
+#!/usr/bin/env python
+
 from setuptools import setup
 
-from distutils import sysconfig
-site_packages_path = sysconfig.get_python_lib()
-
-data_files = [(site_packages_path, ["conf/upseto.pth"])]
-
-# add in case we are running as root
-if os.geteuid() == 0:
-    data_files += [
-        ('/etc/bash_completion.d', ['conf/bash_completion.d/upseto.sh']),
-    ]
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
-    name="upseto",
-    version="1.1",
-    author="Shlomo Matichin",
-    author_email="shlomi@stratoscale.com",
-    description=(
-        "Split your projects across git repos without the disatvantages"
-        " of git submodule"),
-    keywords="git repos repositories python scm",
-    url="http://packages.python.org/upseto",
-    packages=['upseto'],
-    long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Topic :: Utilities",
-    ],
-    install_requires=[
-        "PyYAML==3.11"
-    ],
-    data_files=data_files,
-    scripts=['sh/upseto'],
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
+
+
